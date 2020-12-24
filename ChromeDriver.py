ce0f0b5bab20b5cfe8eb984b86f02ebccc93e432
NackUn <king4521@naver.com>
2020-12-24 09:02:09 +0900

    서버 피씨용 수정사항

--- ChromeDriver.py
+++ ChromeDriver.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get():
-    chrome_driver = '/Users/user/PycharmProjects/parkingpark-macro/parkingpark-macro/venv2/driver/chromedriver'
+    chrome_driver = 'C:/Users/wisemobile5/Desktop/chromedriver_win32/chromedriver.exe'
+    # chrome_driver = webdriver.Chrome(ChromeDriverManager().install())
     return webdriver.Chrome(chrome_driver)
