a55a38fba127c430907f1a103b651da12aee574a
kooym5607 <starkoo06@kookmin.ac.kr>
2020-12-31 13:54:11 +0900

    check_same_car_num 알고리즘 수정2

--- park/ParkUtil.py
+++ park/ParkUtil.py
@@ -115,11 +115,12 @@ def check_same_car_num(parkId, oriCarNum, driver):
         print("나누기전 : " + td_car_num_0)
         td_car_num_1 = re.sub('<.+?>', '', td_car_num_0, 0, re.I | re.S)
         td_car_num_2 = td_car_num_1.strip()
+        td_car_num_3 = td_car_num_2.split('\n')
         global td_car_num
-        if(len(td_car_num_2.split(' '))>1):
-            td_car_num = td_car_num_2.split(' ')[0][-7:]
+        if(len(td_car_num_3[0].split(' '))>1):
+            td_car_num = td_car_num_3[0].split(' ')[0][-7:]
         else:
-            td_car_num = td_car_num_2[-7:]
+            td_car_num = td_car_num_3[0][-7:]
 
         print("검색된 차량번호 : " + td_car_num + " == " + "기존 차량번호 : " + oriCarNum + " / " + oriCarNum[-7:])
 
