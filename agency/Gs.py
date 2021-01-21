abbc7a30112a1576a7622d383ed8a8b3e9761fe3
kooym5607 <starkoo06@kookmin.ac.kr>
2021-01-21 11:45:21 +0900

    GS 로그아웃 테스트

--- agency/Gs.py
+++ agency/Gs.py
@@ -173,8 +173,10 @@ gs_need_log_out =[
 def log_out_web(park_id, driver):
     if park_id in gs_need_log_out:
         Util.sleep(1)
-        wait = WebDriverWait(driver, 20)
-        wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"로그아웃"))).click()
+
+        next = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,"//a[@href='/index.php/login/doLogout']")))
+        driver.implicitly_wait(3)
+        next.click()
         # if park_id == Parks.FINANCE_TOWER:
         #     driver.find_element_by_xpath("//*[@id='bodyCSS']/div[1]/div/div[1]/ul/li[3]/a").click()
         # elif park_id == Parks.MAGOK_SPRINGTOWER \
