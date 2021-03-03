2e60c2352fd9a1485d65931c163257c9835e19af
kooym5607 <starkoo06@kookmin.ac.kr>
2021-03-03 15:50:17 +0900

    에이플러스에셋 주석 추가

--- agency/AplusAsset.py
+++ agency/AplusAsset.py
@@ -16,8 +16,11 @@ def web_har_in(target,driver):
     print("parkId = " + str(park_id) + ", " + "searchId = " + search_id)
     print(Colors.BLUE + ticket_name + Colors.ENDC)
 
+    # 에이플러스에셋 프로그램 실행
     app = application.Application(backend="win32").start('AplusAssetEXE/DiscountParking.exe')
     Util.sleep(1)
+
+    # --------로그인--------
     login_dialog = app.window(title_re='로그인')
     Util.sleep(2)
     login_dialog.Edit.type_keys('1')
@@ -26,6 +29,7 @@ def web_har_in(target,driver):
     login_dialog.Button2.click()
     Util.sleep(2)
 
+    # --------할인창--------
     main_dialog = app.window(title_re='원격할인')
     main_dialog.Edit.type_keys(search_id)
     main_dialog.Button2.click()
@@ -33,6 +37,7 @@ def web_har_in(target,driver):
 
     listCount = main_dialog.ListView.item_count()
     if(listCount>0):
+        # --------차 리스트 받아서 하나씩 검사--------
         for i in range(0,listCount):
             carItem = main_dialog.ListView.texts()[i*3+1]
             print(carItem)
@@ -42,6 +47,7 @@ def web_har_in(target,driver):
                 main_dialog.할인처리.click()
                 Util.sleep(1)
 
+                # -------- 할인완료 창 --------
                 final_dialog = app.window(title_re='할인요청')
                 final_dialog.요청.click()
                 Util.sleep(1)
