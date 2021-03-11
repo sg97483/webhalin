701ff912bc5ab1bdbe719661a6e567af6622efd8
kooym5607 <starkoo06@kookmin.ac.kr>
2021-03-11 09:32:47 +0900

    크롬드라이버 경로 변경

--- ChromeDriver.py
+++ ChromeDriver.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 
 def get():
 
-    chrome_driver = 'C:/Users/park/chromedriver/chromedriver.exe'
+    # 크롬드라이버 경로
+    # 에러 뜬다면 해당 컴퓨터 크롬버전과 안맞음(현재 89.~)
+    # 크롬설정가서 업데이트 눌러서 버전확인
+    chrome_driver = './chromedriver.exe'
     # chrome_driver = webdriver.Chrome(ChromeDriverManager().install())
     return webdriver.Chrome(chrome_driver)
