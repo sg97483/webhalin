2514dc4b39648189e1dd8858a2a6b1685159911e
kooym5607 <starkoo06@kookmin.ac.kr>
2021-03-12 10:24:02 +0900

    init

--- webhalin_macro.py
+++ webhalin_macro.py
@@ -80,7 +80,7 @@ def exec_web_har_in(park_type, target, chrome_driver, lotName=None):
     elif park_type.web_har_in(target, chrome_driver):
         logging_info(target)
         in_car_check_db(pid, park_id)
-        # push_fcm_in_car_check(pid)
+        push_fcm_in_car_check(pid)
     else:
         print("실패")
 
