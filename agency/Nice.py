8a38bc7e45edd1a640d265cad8f325243e76665f
kooym5607 <starkoo06@kookmin.ac.kr>
2021-03-17 17:36:18 +0900

    나이스파크 심야권 적용(테스트)

--- agency/Nice.py
+++ agency/Nice.py
@@ -33,63 +33,66 @@ def web_har_in(target, driver):
     print("parkId = " + str(park_id) + ", " + "searchId = " + search_id)
     print(Colors.BLUE + ticket_name + Colors.ENDC)
 
-    if str(ticket_name).endswith('1일권'):
-        driver.get(nice_url)
+
+    driver.get(nice_url)
+    time.sleep(3)
+
+    # 처음 로그인 팝업 확인
+    try:
+        WebDriverWait(driver, 3).until(EC.alert_is_present(),'Timed out waiting for PA creation ' +'confirmation popup to appear.')
+        driver.switch_to.alert.accept()
         time.sleep(3)
-        try:
-            # wait for the alert to show up
-            WebDriverWait(driver, 3).until(EC.alert_is_present(),
-                                           'Timed out waiting for PA creation ' +
-                                           'confirmation popup to appear.')
-            # if it does
-            driver.switch_to.alert.accept()
-            time.sleep(3)
-        except TimeoutException:
-            print("no alert")
-        try:
-            driver.find_element_by_id("login_id").send_keys("admin")
-            driver.find_element_by_id("login_pw").send_keys("!@park0413")
-            driver.find_element_by_xpath("""//*[@id="login_fs"]/input[3]""").click()
-            driver.implicitly_wait(3)
-        except Exception as ex:
-            print(Colors.RED + str(ex) + Colors.ENDC)
+    except TimeoutException:
+        print("no alert")
 
-        driver.get(nice_url)
+    # 로그인
+    try:
+        driver.find_element_by_id("login_id").send_keys("admin")
+        driver.find_element_by_id("login_pw").send_keys("!@park0413")
+        driver.find_element_by_xpath("""//*[@id="login_fs"]/input[3]""").click()
+        driver.implicitly_wait(3)
+    except Exception as ex:
+        print(Colors.RED + str(ex) + Colors.ENDC)
 
-        driver.find_element_by_name('stx').send_keys(search_id)
-        driver.find_element_by_xpath('//*[@id="fsearch"]/input').click()
-        # try:
-        #     searched_car_number = driver.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td[1]/font/b').text
-        # except NoSuchElementException as ex:
-        #     print("차량이 검색되지 않음\n" + ex)
+    driver.get(nice_url)
 
-        time.sleep(2)
-        count = len(driver.find_elements_by_xpath("/html/body/table[1]/tbody/tr"))
+    driver.find_element_by_name('stx').send_keys(search_id)
+    driver.find_element_by_xpath('//*[@id="fsearch"]/input').click()
 
-        for index in range(2, count+1):
-            sale_table = driver.find_element_by_xpath("/html/body/table[1]/tbody/tr[" + str(index) + "]")
-            searched_car_number = driver.find_element_by_xpath("/html/body/table[1]/tbody/tr[" + str(index) + "]/td[1]").text
-            print("나누기전 : " + searched_car_number)
-            td_car_num_1 = re.sub('<.+?>', '', searched_car_number, 0, re.I | re.S)
-            td_car_num_2 = td_car_num_1.strip()
-            td_car_num_3 = td_car_num_2.split('\n')
+    time.sleep(2)
+    count = len(driver.find_elements_by_xpath("/html/body/table[1]/tbody/tr"))
 
-            global td_car_num
-            if len(td_car_num_3[0].split(' ')) > 1:
-                td_car_num = td_car_num_3[0].split(' ')[0][-7:]
-            else:
-                td_car_num = td_car_num_3[0][-7:]
+    # 차량리스트 검색 for문
+    for index in range(2, count+1):
 
-            print("검색된 차량번호 : " + td_car_num + " == " + "기존 차량번호 : " + ori_car_num + " / " + ori_car_num[-7:])
+        sale_table = driver.find_element_by_xpath("/html/body/table[1]/tbody/tr[" + str(index) + "]")
+        searched_car_number = driver.find_element_by_xpath("/html/body/table[1]/tbody/tr[" + str(index) + "]/td[1]").text
+        print("나누기전 : " + searched_car_number)
+        td_car_num_1 = re.sub('<.+?>', '', searched_car_number, 0, re.I | re.S)
+        td_car_num_2 = td_car_num_1.strip()
+        td_car_num_3 = td_car_num_2.split('\n')
 
-            if ori_car_num[-7:] == td_car_num or ori_car_num == td_car_num:
-                print("차량 번호 같음")
-                print("index = " + str(index))
-                sale_count = len(driver.find_elements_by_xpath("/html/body/table[1]/tbody/tr[" + str(index) + "]/td[11]/b"))
-                print("sale_count : " + str(sale_count))
-                for sale_index in range(1, sale_count+1):
-                    sale_text = driver.find_element_by_xpath("/html/body/table[1]/tbody/tr[" + str(index) + "]/td[11]/b["+str(sale_index)+"]")
-                    print(sale_text.text)
+        global td_car_num
+        if len(td_car_num_3[0].split(' ')) > 1:
+            td_car_num = td_car_num_3[0].split(' ')[0][-7:]
+        else:
+            td_car_num = td_car_num_3[0][-7:]
+
+        print("검색된 차량번호 : " + td_car_num + " == " + "기존 차량번호 : " + ori_car_num + " / " + ori_car_num[-7:])
+
+        if ori_car_num[-7:] == td_car_num or ori_car_num == td_car_num:
+            print("차량 번호 같음")
+            print("index = " + str(index))
+            sale_count = len(driver.find_elements_by_xpath("/html/body/table[1]/tbody/tr[" + str(index) + "]/td[11]/b"))
+            print("sale_count : " + str(sale_count))
+
+            # 할인권리스트 for문
+            for sale_index in range(1, sale_count+1):
+                sale_text = driver.find_element_by_xpath("/html/body/table[1]/tbody/tr[" + str(index) + "]/td[11]/b["+str(sale_index)+"]")
+                print(sale_text.text)
+
+                # 1일권
+                if str(ticket_name).endswith('1일권'):
                     if (sale_text.text == '입차일 당일'):
                         sale_text.click()
                         try:
@@ -101,68 +104,58 @@ def web_har_in(target, driver):
                     else:
                         continue
 
-            else:
-                print(Colors.MARGENTA + "차량번호가 틀립니다." + Colors.ENDC)
-                continue
-
-
-            # for trs in sale_table.find_elements_by_tag_name("tr"):
-            #     print(trs)
-            #     # 테이블 헤더 제외
-            #     if index==0:
-            #         index = index+1
-            #         continue
-            #
-            #     # 검색 결과 <tr>
-            #     elif index > 0:
-            #         tds = trs.find_element_by_tag_name("td")
-            #         searched_car_number = tds.text
-            #         print("나누기전 : " + searched_car_number)
-            #         td_car_num_1 = re.sub('<.+?>', '', searched_car_number, 0, re.I | re.S)
-            #         td_car_num_2 = td_car_num_1.strip()
-            #         td_car_num_3 = td_car_num_2.split('\n')
-            #
-            #         global td_car_num
-            #         if len(td_car_num_3[0].split(' ')) > 1:
-            #             td_car_num = td_car_num_3[0].split(' ')[0][-7:]
-            #         else:
-            #             td_car_num = td_car_num_3[0][-7:]
-            #
-            #         print("검색된 차량번호 : " + td_car_num + " == " + "기존 차량번호 : " + ori_car_num + " / " + ori_car_num[-7:])
-            #
-            #         if ori_car_num[-7:] == td_car_num or ori_car_num == td_car_num:
-            #             print("차량 번호 같음")
-            #             for sale_b in tds[10].find_elements_by_tag_name('b'):
-            #                 if(sale_b.text == '입차일 당일'):
-            #                     driver.find_element_by_css_selector(sale_b).click()
-            #                     try:
-            #                         driver.find_element_by_css_selector(
-            #                             "#modal-window > div > div > div.modal-buttons > a").click()
-            #                     except Exception as ex:
-            #                         print(Colors.RED + str(ex) + Colors.ENDC)
-            #                     return True
-            #
-            #         else:
-            #             print(Colors.MARGENTA + "차량번호가 틀립니다." + Colors.ENDC)
-            #             index = index + 1
-            #             continue
-
+                # 심야권
+                elif str(ticket_name).endswith('심야권') or str(ticket_name).endswith('야간권'):
+                    if(str(sale_text.text).startswith('17') and str(sale_text.text)[6] == '0'):
+                        sale_text.click()
+                        try:
+                            driver.find_element_by_css_selector(
+                                "#modal-window > div > div > div.modal-buttons > a").click()
+                        except Exception as ex:
+                            print(Colors.RED + str(ex) + Colors.ENDC)
+                        return True
+                    elif(str(sale_text.text).startswith('18') and str(sale_text.text)[6] == '0'):
+                        sale_text.click()
+                        try:
+                            driver.find_element_by_css_selector(
+                                "#modal-window > div > div > div.modal-buttons > a").click()
+                        except Exception as ex:
+                            print(Colors.RED + str(ex) + Colors.ENDC)
+                        return True
+                    elif (str(sale_text.text).startswith('19') and str(sale_text.text)[6] == '0'):
+                        sale_text.click()
+                        try:
+                            driver.find_element_by_css_selector(
+                                "#modal-window > div > div > div.modal-buttons > a").click()
+                        except Exception as ex:
+                            print(Colors.RED + str(ex) + Colors.ENDC)
+                        return True
+                    elif (str(sale_text.text).startswith('20') and str(sale_text.text)[6] == '0'):
+                        sale_text.click()
+                        try:
+                            driver.find_element_by_css_selector(
+                                "#modal-window > div > div > div.modal-buttons > a").click()
+                        except Exception as ex:
+                            print(Colors.RED + str(ex) + Colors.ENDC)
+                        return True
+                    elif (str(sale_text.text).startswith('22') and str(sale_text.text)[6:].startswith('10')): # 나이스파크 월피점
+                        sale_text.click()
+                        try:
+                            driver.find_element_by_css_selector(
+                                "#modal-window > div > div > div.modal-buttons > a").click()
+                        except Exception as ex:
+                            print(Colors.RED + str(ex) + Colors.ENDC)
+                        return True
+                    else:
+                        continue
+                else:
+                    continue
+            print("할인권 적용 실패")
             return False
+        else:
+            print(Colors.MARGENTA + "차량번호가 틀립니다." + Colors.ENDC)
+            continue
+
         return False
+    return False
 
-
-    else:
-        print("나이스 - (평일/주말)1일권이 아님")
-        return False
-
-# sale_table = temp_table.find_all('b')
-
-# for list in sale_table:
-#     print(list.text)
-#     if (list.text == '입차일 당일'):
-#         list.click()
-#         try:
-#             driver.find_element_by_css_selector("#modal-window > div > div > div.modal-buttons > a").click()
-#         except Exception as ex:
-#             print(Colors.RED + str(ex) + Colors.ENDC)
-#         return True
