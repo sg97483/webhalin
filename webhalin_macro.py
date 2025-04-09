00cdc6710e63d07ed65d786bfeb6826d1ad07522
sg97483 <sg97483@gmail.com>
2025-04-09 15:43:51 +0900

    250410-2:all_fix

--- webhalin_macro.py
+++ webhalin_macro.py
@@ -100,56 +100,56 @@ def web_har_in(target):
     park_type = ParkType.get_park_type(park_id)
 
     # 아마노코리아
-    #if  park_type == ParkType.NEW_AMANO:
-    #    exec_web_har_in(NewAmano, target, driver)
-    #    return True
+    if  park_type == ParkType.NEW_AMANO:
+        exec_web_har_in(NewAmano, target, driver)
+        return True
 
     # 나이스파크
-    #elif park_type == ParkType.NICE_NEW:
-    #   exec_web_har_in(NiceNew, target, driver)
-    #   return True
+    elif park_type == ParkType.NICE_NEW:
+       exec_web_har_in(NiceNew, target, driver)
+       return True
 
     # 하이파킹
-    #elif park_type == ParkType.HIGH_MHP_OPT:
-    #    exec_web_har_in(HighMhpOpt, target, driver)
-    #    return True
+    elif park_type == ParkType.HIGH_MHP_OPT:
+        exec_web_har_in(HighMhpOpt, target, driver)
+        return True
 
-    #if park_type == ParkType.HIGH_CITY:
-    #    exec_web_har_in(HighCity, target, driver)
-    #    print(f"park_id: {park_id}, park_type: {park_type}, expected: {ParkType.HIGH_CITY}")
-    #    return True
+    elif park_type == ParkType.HIGH_CITY:
+        exec_web_har_in(HighCity, target, driver)
+        print(f"park_id: {park_id}, park_type: {park_type}, expected: {ParkType.HIGH_CITY}")
+        return True
 
     #아이파킹사이트(운영업체혼합)
-    #elif park_type == ParkType.I_PARKING:
-    #    exec_web_har_in(Iparking, target, driver)
-    #    print(f"park_id: {park_id}, park_type: {park_type}, expected: {ParkType.I_PARKING}")
-    #    return True
+    elif park_type == ParkType.I_PARKING:
+        exec_web_har_in(Iparking, target, driver)
+        print(f"park_id: {park_id}, park_type: {park_type}, expected: {ParkType.I_PARKING}")
+        return True
 
     # KMPARK 전용사이트
-    #elif park_type == ParkType.NEW_KMPARK:
-    #    exec_web_har_in(NewKmpark, target, driver)
-    #    return True
+    elif park_type == ParkType.NEW_KMPARK:
+        exec_web_har_in(NewKmpark, target, driver)
+        return True
 
-    #elif park_type == ParkType.CENTER_SQUARE:
-    #    exec_web_har_in(CenterSquare, target, driver)
-    #    return True
+    elif park_type == ParkType.CENTER_SQUARE:
+        exec_web_har_in(CenterSquare, target, driver)
+        return True
 
-    #elif park_type == ParkType.ETC:
-    #    exec_web_har_in(Etc, target, driver)
-    #    return True
+    elif park_type == ParkType.ETC:
+        exec_web_har_in(Etc, target, driver)
+        return True
 
-    #elif park_type == ParkType.IP_TIME:
-    #    exec_web_har_in(Iptime, target, driver)
-    #    return True
+    elif park_type == ParkType.IP_TIME:
+        exec_web_har_in(Iptime, target, driver)
+        return True
 
-    if park_type == ParkType.GS:
+    elif park_type == ParkType.GS:
         exec_web_har_in(Gs, target, driver)
         print(f"park_id: {park_id}, park_type: {park_type}, expected: {ParkType.GS}")
         return True
 
-    #elif park_type == ParkType.AJ_PARK:
-    #    exec_web_har_in(AJpark, target, driver)
-    #    return True
+    elif park_type == ParkType.AJ_PARK:
+        exec_web_har_in(AJpark, target, driver)
+        return True
 
     #elif park_type == ParkType.GS2:
     #    exec_web_har_in(Gs, target, driver)
@@ -163,8 +163,6 @@ def web_har_in(target):
     #elif park_type == ParkType.BLUE:
     #    exec_web_har_in(Blue, target, driver)
     #    return True
-
-
 
 
 
