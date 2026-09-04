1f4bb7e6b0a1b1d498ee71c1568782a1684c855a
sg97483 <sg97483@gmail.com>
2026-09-04 09:20:38 +0900

    260904-:all_fix1

--- agency/NiceNew.py
+++ agency/NiceNew.py
@@ -2037,6 +2037,24 @@ def web_har_in(target, driver):
                 return handle_invalid_ticket(driver)
 
 
+        elif park_id == 29688:
+
+            if ticket_name in ["평일 당일권(카리프트)", "휴일 당일권(카리프트)"]:
+                return select_discount_and_confirm(
+                    driver,
+                    "//*[@id='mf_wfm_body_gen_dcTkList_1_discountTkGrp']"
+                )
+
+            elif ticket_name in ["평일 3시간권(카리프트)", "휴일 3시간권(카리프트)"]:
+                return select_discount_and_confirm(
+                    driver,
+                    "//*[@id='mf_wfm_body_gen_dcTkList_0_discountTkGrp']"
+                )
+
+            else:
+                return handle_invalid_ticket(driver)
+
+
         elif park_id == 29514:
 
             if ticket_name == "평일 심야권":
