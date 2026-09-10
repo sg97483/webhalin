be54d4ba56ee0cbd8ff61ff13fc99ea014db0ace
sg97483 <sg97483@gmail.com>
2026-09-10 09:23:07 +0900

    260910-:all_fix1

--- agency/NiceNew.py
+++ agency/NiceNew.py
@@ -1022,17 +1022,17 @@ def web_har_in(target, driver):
             if ticket_name == "평일1일권":
                 return select_discount_and_confirm(
                     driver,
-                    "//*[@id='mf_wfm_body_gen_dcTkList_1_discountTkGrp']"
+                    "//*[@id='mf_wfm_body_gen_dcTkList_2_discountTkGrp']"
                 )
             elif ticket_name == "평일 심야권":
                 return select_discount_and_confirm(
                     driver,
-                    "//*[@id='mf_wfm_body_gen_dcTkList_0_discountTkGrp']"
+                    "//*[@id='mf_wfm_body_gen_dcTkList_1_discountTkGrp']"
                 )
             elif ticket_name == "휴일 당일권":
                 return select_discount_and_confirm(
                     driver,
-                    "//*[@id='mf_wfm_body_gen_dcTkList_1_discountTkGrp']"
+                    "//*[@id='mf_wfm_body_gen_dcTkList_2_discountTkGrp']"
                 )
             else:
                 return handle_invalid_ticket(driver)
